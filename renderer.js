80193ac5b04f70bf7f515f6b19d3478cfb136819
Mathieu Débit <mathieu.debit@gmail.com>
2018-11-22 17:53:17 +0100

    Bug: Window not positioned correctly when using setPosition()

--- renderer.js
+++ renderer.js
@@ -1,3 +1,12 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const mainWindow = require('electron').remote.getCurrentWindow();
+const resetPositionButton = document.getElementById('resetPositionButton');
+const positionInfo = document.getElementById('positionInfo');
+
+resetPositionButton.onclick = () => {
+  mainWindow.setPosition(0, 0);
+  positionInfo.innerHTML = mainWindow.getPosition();
+};
